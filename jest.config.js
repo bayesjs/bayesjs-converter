4bfa1800717f3cf5bd85eec84a9b110d87287ab5
fhelwanger <fhelwanger@gmail.com>
2017-10-11 23:32:57 -0300

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json']
+};
