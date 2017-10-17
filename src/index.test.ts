02f84342db04e44defa618ed579d842283908ab3
fhelwanger <fhelwanger@gmail.com>
2017-10-17 00:23:51 -0200

    Handle accents

--- src/index.test.ts
+++ src/index.test.ts
@@ -15,6 +15,23 @@ it('converts network correctly', () => {
   expect(bayesnet).toMatchSnapshot();
 });
 
+it('handles accents correctly', () => {
+  expect(lexer('"\\303\\200"')).toEqual([{ type: 'string', value: 'À' }]);
+  expect(lexer('"\\303\\203"')).toEqual([{ type: 'string', value: 'Ã' }]);
+  expect(lexer('"\\303\\207"')).toEqual([{ type: 'string', value: 'Ç' }]);
+  expect(lexer('"\\303\\211"')).toEqual([{ type: 'string', value: 'É' }]);
+  expect(lexer('"\\303\\212"')).toEqual([{ type: 'string', value: 'Ê' }]);
+  expect(lexer('"\\303\\215"')).toEqual([{ type: 'string', value: 'Í' }]);
+  expect(lexer('"\\303\\223"')).toEqual([{ type: 'string', value: 'Ó' }]);
+  expect(lexer('"\\303\\240"')).toEqual([{ type: 'string', value: 'à' }]);
+  expect(lexer('"\\303\\243"')).toEqual([{ type: 'string', value: 'ã' }]);
+  expect(lexer('"\\303\\247"')).toEqual([{ type: 'string', value: 'ç' }]);
+  expect(lexer('"\\303\\251"')).toEqual([{ type: 'string', value: 'é' }]);
+  expect(lexer('"\\303\\252"')).toEqual([{ type: 'string', value: 'ê' }]);
+  expect(lexer('"\\303\\255"')).toEqual([{ type: 'string', value: 'é' }]);
+  expect(lexer('"\\303\\263"')).toEqual([{ type: 'string', value: 'ó' }]);
+});
+
 const readNetwork = (networkName: string) => {
   const networkPath = path.join(__dirname, `../networks/${networkName}.net`);
   const networkSource = fs.readFileSync(networkPath).toString();
--- src/lexer.ts
+++ src/lexer.ts
@@ -7,7 +7,20 @@ const regexIdentifier = /[a-z0-9_]/i;
 export default (source: string): Array<Token> => {
   const tokens: Array<Token> = [];
 
+  source = source.replace(/\\303\\200/g, 'À');
+  source = source.replace(/\\303\\203/g, 'Ã');
+  source = source.replace(/\\303\\207/g, 'Ç');
+  source = source.replace(/\\303\\211/g, 'É');
+  source = source.replace(/\\303\\212/g, 'Ê');
+  source = source.replace(/\\303\\215/g, 'Í');
+  source = source.replace(/\\303\\223/g, 'Ó');
+  source = source.replace(/\\303\\240/g, 'à');
   source = source.replace(/\\303\\243/g, 'ã');
+  source = source.replace(/\\303\\247/g, 'ç');
+  source = source.replace(/\\303\\251/g, 'é');
+  source = source.replace(/\\303\\252/g, 'ê');
+  source = source.replace(/\\303\\255/g, 'é');
+  source = source.replace(/\\303\\263/g, 'ó');
 
   let currentIndex = 0;
 
